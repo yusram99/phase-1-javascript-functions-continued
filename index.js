59886a2bf6e14a488234dfbdf1623f6f352c364c
Your Name <youremail@example.com>
2023-06-18 16:02:44 +0300

    commit message

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  const mondayWork = function (activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  function wrapAdjective(flair = '*') {
+    return function (adjective) {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  module.exports = {
+    saturdayFun,
+    mondayWork,
+    wrapAdjective
+  };
+  
+  
+  
+  
+  
+  
+  
